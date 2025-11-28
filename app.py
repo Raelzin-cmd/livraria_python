d9c621467ff2da83412a35c09f47e2dbb78c0f2a
Israel Almeida <almeidev.rael@gmail.com>
2025-11-28 15:57:40 -0300

    Folder Controllers added

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, make_response
+from flask import Flask
+from controllers import authors
 
 app = Flask(__name__)
 
-@app.route('/')
-
-def test():
-    return make_response('Tudo certo')
+# Cria uma rota e importa função
+app.add_url_rule('/', view_func=authors.test)
 
 app.run(port=3000, debug=True)
---
+++ controllers/authors.py
@@ -0,0 +1,4 @@
+from flask import make_response
+
+def test():
+    return make_response('Tudo certo')
